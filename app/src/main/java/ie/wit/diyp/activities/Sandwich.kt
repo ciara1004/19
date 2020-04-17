5cb8e6a8a220f7f5cd5033fa7096c85bbcdb5b61
Ciara Cox <ciara.1004@hotmail.com>
2020-04-17 19:42:27 +0100

    Initial commit

---
+++ app/src/main/java/ie/wit/diyp/activities/Sandwich.kt
@@ -0,0 +1,37 @@
+package ie.wit.diyp.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import ie.wit.diyp.R
+import ie.wit.diyp.main.DIYPApp
+import kotlinx.android.synthetic.main.content_default_bar.*
+
+class Sandwich : AppCompatActivity() {
+    lateinit var app: DIYPApp
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_sandwich)
+        setSupportActionBar(toolbar)
+
+        val barButton = findViewById<Button>(R.id.barButton)
+        barButton.setOnClickListener{
+            val intent = Intent(this, Bar::class.java)
+            startActivity(intent)
+        }
+
+        val barfoodButton = findViewById<Button>(R.id.barfoodButton)
+        barfoodButton.setOnClickListener{
+            val intent = Intent(this, Barfood::class.java)
+            startActivity(intent)
+        }
+
+        val defaultMenu = findViewById<Button>(R.id.defaultMenu)
+        defaultMenu.setOnClickListener{
+            val intent = Intent(this, Barfood::class.java)
+            startActivity(intent)
+        }
+    }
+}
